23a378f721587ef31c417f1ed217275448bdf214
pasitpk <pasit.j1995@gmail.com>
2021-09-05 15:00:28 +0700

    change input value format

--- utils/clinic_utils.py
+++ utils/clinic_utils.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 
 def str_to_dt(date_str, time_str):
-    return datetime.strptime(date_str + ' ' + time_str, '%Y-%m-%d %H:%M')
+    return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S.%fZ')
 
 
 def cal_duration(dt1, dt2, div=60):
@@ -15,7 +15,7 @@ def extract_handcraft_features(data):
 
     arrival_dt = str_to_dt(data['arrival_date'], data['arrival_time'])
 
-    if data['onset'] == 'known_onset':
+    if data['onset'] == 'clearOnset':
         clear_onset_dt = str_to_dt(
             data['clear_onset_date'], data['clear_onset_time'])
         features['duration lsn (minute)'] = cal_duration(arrival_dt, clear_onset_dt)
@@ -27,16 +27,16 @@ def extract_handcraft_features(data):
         features['duration lsn (minute)'] = cal_duration(arrival_dt, lsn_dt)
         features['duration fsa (minute)'] = cal_duration(arrival_dt, fsa_dt)
 
-    features['gradual onset'] = 1 if data['time_course'] == 'gradual' else 0
-    features['peak clear onset'] = 1 if data['time_course'] == 'peak_at_onset' else 0
+    features['gradual onset'] = 1 if data['time_course'] == 'Gradual' else 0
+    features['peak clear onset'] = 1 if data['time_course'] == 'peakAtOnset' else 0
 
     features['cortical lobe sign'] = 1 if (
         (int(data['cc_aphasia']) == 1) or
-        (int(data['nihss_1b']) == 2) or
-        (int(data['nihss_2']) == 2) or
-        (int(data['nihss_3']) > 0) or
-        (int(data['nihss_9']) > 0) or
-        (int(data['nihss_11']) > 0)
+        (data['nihss_1b'] == 'NoneCorrect') or
+        (data['nihss_2'] == 'ForcedDeviation') or
+        (data['nihss_3'] != 'NoVisualLoss') or
+        (data['nihss_9'] != 'No aphasia') or
+        (data['nihss_11'] != 'NoNeglect')
     ) else 0
 
     features['metabolic syndrome'] = 1 if (
